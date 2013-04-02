b8629d1d78cce0a0a677c32a4ad560d1a7f15d7d
Frederik Lauridsen <rip_bitzer@hotmail.com>
2013-04-02 16:04:38 +0300

    Update connectioninfo.php

--- source/connectioninfo.php
+++ source/connectioninfo.php
@@ -3,8 +3,8 @@
 class ConnectionInfo {
 
   //Google account that owns the Fusion Table
-  public static $google_username = "ripbitzer@gmail.com";
-  public static $google_password = "kanelsnegl";
+  public static $google_username = "xxxxxxxxx";
+  public static $google_password = "xxxxxxxxx";
   public static $googleApiKey = "AIzaSyDeptjsglykULgvQLHfJV1kN3hWULIY_u0"; //get yours here: https://code.google.com/apis/console/
   public static $fusionTableId = "1hHmhIzp3h8S0BNJ7hA2Je2GogrTEFq4JZTm8J9Y"; //must be encrypted!
   
