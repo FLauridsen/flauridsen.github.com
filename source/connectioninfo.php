e830f3e1ebe9312bffc0cffa7af44f6909891c93
Frederik Lauridsen <rip_bitzer@hotmail.com>
2013-04-02 14:09:54 +0200

    added ft-table

---
+++ source/connectioninfo.php
@@ -0,0 +1,12 @@
+<php
+
+class ConnectionInfo {
+
+  //Google account that owns the Fusion Table
+  public static $google_username = "ripbitzer@gmail.com";
+  public static $google_password = "kanelsnegl";
+  public static $googleApiKey = "AIzaSyDeptjsglykULgvQLHfJV1kN3hWULIY_u0"; //get yours here: https://code.google.com/apis/console/
+  public static $fusionTableId = "1hHmhIzp3h8S0BNJ7hA2Je2GogrTEFq4JZTm8J9Y"; //must be encrypted!
+  
+}
+>
